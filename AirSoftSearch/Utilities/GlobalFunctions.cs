437310569374f152a8701dc76c39e5e6728d77df
wrecknralph <thompson.bradley@comcast.net>
2024-03-06 12:54:31 -0800

    added evike site to search and added image of web site

---
+++ AirSoftSearch/Utilities/GlobalFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace AirSoftSearch.Utilities
+{
+    public static class GlobalFunctions
+    {
+        public static string CreateUrlImage(string sUrl)
+        {          
+
+            string url = $"https://api.screenshotone.com/take?url={WebUtility.UrlEncode(sUrl)}&full_page=false&format=png&block_cookie_banners=true&access_key=VI6y_F5Ub0cNRw";
+            return url;
+        }        
+    }
+}
